7b34e89ee651aeea9656634521f9db5358d96c57
khalilholberton <1071@holbertonschool.com>
2019-10-29 11:48:03 +0000

    fix

---
+++ holberton.h
@@ -0,0 +1,32 @@
+#ifndef _PRINT_F
+#define _PRINT_F
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct convert - defines a structure for symbols and functions
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+struct convert
+{
+char *op;
+int (*f)(va_list);
+};
+typedef struct convert conver_t;
+
+/*Functions*/
+int converter(const char *format,
+conver_t list_of_function[], va_list arg_list);
+int _printf(const char *format, ...);
+int _putchar(char);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+int print_integer(va_list);
+int print_number(va_list);
+
+#endif
